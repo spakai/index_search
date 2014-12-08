fd9102b26392e0e81d0ece907f07998e2d967344
spakai <spakai@gmail.com>
2014-12-08 18:55:33 +0800

    Add tests

--- TableTest.cpp
+++ TableTest.cpp
@@ -5,9 +5,9 @@
 
 using namespace testing;
 
-TEST(AToken,ReadsTillEOF) {
+TEST(TableIterator,ReadsTillEOF) {
     std::ifstream in;
-    in.open("sample.csv"); 
+    in.open("../csv/sample.csv"); 
     
     Table t(in);
 
@@ -16,4 +16,19 @@ TEST(AToken,ReadsTillEOF) {
     std::for_each(t,Table(), [&numberOfRows](const std::string& record){ ++numberOfRows; });
  
     ASSERT_THAT(numberOfRows,Eq(5));
+}
+
+TEST(TableIterator,ReadLastRow) {
+
+    std::ifstream in;
+    in.open("../csv/sample.csv"); 
+    
+    Table t(in);
+
+    std::string line; 
+
+    std::for_each(t,Table(), [&line](const std::string& record){ line = record; });
+ 
+    ASSERT_THAT(line,Eq("12,DF,ER,AD,JK"));
+
 }
