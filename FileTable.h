1a1a5f5bd6eb726d28f7540326774ff72a2a4cef
Prakash S <spakai@gmail.com>
2015-02-08 20:47:20 +0800

    Introduced superclass Table

--- FileTable.h
+++ FileTable.h
@@ -1,50 +1,11 @@
-#include <vector>
-#include <string>
-#include <fstream>
-#include <stdexcept>
+#include "Table.h"
 
-class FileTable {
+class FileTable : public Table{
     public:
 		FileTable() {};
 		~FileTable() {};
         FileTable(const std::string& filename);
         void init(const std::string& filename);
 		int getNumberOfRows();
-		std::string& getRow(int index);
-
-    class Iterator {
-        public:
-            Iterator(FileTable &table) : table(&table), index(0), eof(false) {}
-            Iterator() : eof(true) {}
-            Iterator& operator++() {
-                ++index;   
-                if(index == table->getNumberOfRows()) {
-                    eof = true;    
-                }
-                return (*this);
-            }
-            Iterator operator++(int);
-            std::string operator*() {
-                return table->getRow(index);
-            }
-            std::string& operator->();
-            bool operator==(const Iterator& other) {
-                return (eof == other.eof);
-            }
-            bool operator!=(const Iterator& other) {
-                return (eof != other.eof);
-            }
-
-
-        private:
-            FileTable *table;
-            int index;
-            bool eof;
-    };
-
-    private:
-        std::ifstream csv_file;
-        std::vector<int> offsets;
-        std::vector<int> lengths;
-		std::string currentLine;
+		virtual std::string& getRow(int index);
 };
---
+++ Table.h
@@ -0,0 +1,46 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+
+class Table {
+    public:
+		virtual int getNumberOfRows()=0;
+		virtual std::string& getRow(int index)=0;
+
+    class Iterator {
+        public:
+            Iterator(Table &table) : table(&table), index(0), eof(false) {}
+            Iterator() : eof(true) {}
+            Iterator& operator++() {
+                ++index;   
+                if(index == table->getNumberOfRows()) {
+                    eof = true;    
+                }
+                return (*this);
+            }
+            Iterator operator++(int);
+            std::string operator*() {
+                return table->getRow(index);
+            }
+            std::string& operator->();
+            bool operator==(const Iterator& other) {
+                return (eof == other.eof);
+            }
+            bool operator!=(const Iterator& other) {
+                return (eof != other.eof);
+            }
+
+
+        protected:
+            Table *table;
+            int index;
+            bool eof;
+    };
+
+    protected:
+        std::ifstream csv_file;
+        std::vector<int> offsets;
+        std::vector<int> lengths;
+		std::string currentLine;
+};
