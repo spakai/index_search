56b1dc3bcaf6e34214787bafafb1ad0a417690c4
spakai <spakai@gmail.com>
2015-02-11 11:12:43 +0800

    Adjust identation

--- FileTable.cpp
+++ FileTable.cpp
@@ -5,8 +5,8 @@ FileTable::FileTable(const std::string& filename) {
 }
 
 void FileTable::init(const std::string& filename) {
-	csv_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);	
-	csv_file.open(filename);
+    csv_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);	
+    csv_file.open(filename);
 
 	try {
 		int offset {0};
@@ -39,7 +39,7 @@ std::string& FileTable::getRow(int index) {
     csv_file.seekg(offsets[index], std::ios_base::beg);
     char * buffer = new char[lengths[index]];
     csv_file.read(buffer, lengths[index]);
-	currentLine.assign(buffer,lengths[index]);
+    currentLine.assign(buffer,lengths[index]);
     delete[] buffer;
     return currentLine;
 }
--- FileTable.h
+++ FileTable.h
@@ -2,10 +2,10 @@
 
 class FileTable : public Table{
     public:
-		FileTable() {};
-		~FileTable() {};
+        FileTable() {};
+        ~FileTable() {};
         FileTable(const std::string& filename);
         void init(const std::string& filename);
-		int getNumberOfRows();
-		virtual std::string& getRow(int index);
+        virtual int getNumberOfRows();
+        virtual std::string& getRow(int index);
 };
