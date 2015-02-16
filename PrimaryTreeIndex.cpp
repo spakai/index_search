8aaa575de13f024e42de4ab7e92eae5a28758378
Prakash S <spakai@gmail.com>
2015-02-16 22:51:42 +0800

    Use emplace instead of insert

--- PrimaryTreeIndex.cpp
+++ PrimaryTreeIndex.cpp
@@ -3,8 +3,8 @@
 void PrimaryTreeIndex::buildIndex(Table & table, int column) {
     int rowno = 0;
     for(auto currentRow : table) {
-        index.insert( std::pair<std::string, int>(currentRow[column], rowno++) );
-	}
+        index.emplace(currentRow[column], rowno++);
+    }
 }
 
 int PrimaryTreeIndex::size() const {
