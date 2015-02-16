1bb2b7139aa357df70422604c824e931146c88a5
spakai <spakai@gmail.com>
2015-02-16 17:22:51 +0800

    Refactor index insert code

--- PrimaryTreeIndex.cpp
+++ PrimaryTreeIndex.cpp
@@ -4,7 +4,7 @@ void PrimaryTreeIndex::buildIndex(Table & table, int column) {
     int offset_index=0;
     for(auto it=table.begin(); it!=table.end(); ++it) {
         // key is row column and value is offset's index or basically row no
-        index[(*it)[column]] = offset_index++; 
+        index.insert( std::pair<std::string, int>((*it)[column], offset_index++) );
 	}
 }
 
