f2aaa6b143e152f4cc7ddfa30b793f592b28ca1c
Prakash S <spakai@gmail.com>
2015-02-17 22:47:43 +0800

    Added Exact and Best match logic

--- PrimaryTreeIndex.cpp
+++ PrimaryTreeIndex.cpp
@@ -14,7 +14,22 @@ int PrimaryTreeIndex::size() const {
 int PrimaryTreeIndex::lookup(const std::string& key) {
     auto bounds = index.equal_range(key);
     auto lower_bound = bounds.first;
+    auto upper_bound = bounds.second;
+
+    if(lower_bound == index.end()) {
+        std::map<std::string,int>::const_reverse_iterator rbegin(upper_bound); 
+        std::map<std::string,int>::const_reverse_iterator rend(index.begin()); 
+        for(auto it = rbegin; it!=rend; it++) {
+            auto idx = key.find(it->first);
+            if(idx != std::string::npos) {
+                return it->second;
+            } 
+        }
+        
+    }
+
     if(lower_bound->first == key) {
         return lower_bound->second; 
     }
+
 }
