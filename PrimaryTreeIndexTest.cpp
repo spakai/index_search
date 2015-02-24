61caa9cc2c0dbbf81469142a8aafd32499ee6d88
Prakash S <spakai@gmail.com>
2015-02-24 23:43:39 +0800

    Test Best Match to do Exact Match too

--- PrimaryTreeIndexTest.cpp
+++ PrimaryTreeIndexTest.cpp
@@ -14,13 +14,22 @@ TEST(PrimaryTreeIndex,GetSizeofIndex) {
     ASSERT_THAT(index.size(), Eq(56));      
 }
 
-TEST(PrimaryTreeIndex,ExactMatchLookup) {
+TEST(PrimaryTreeIndex,ExactMatchWhenExactMatchLookupIsCalled) {
     FileTable ft;
     ft.init("../csv/bnumber2.csv");
     PrimaryTreeIndex index;
     index.buildIndex(ft, 0);
     ASSERT_THAT(index.exactMatch("01386"), Eq(25));      
 }
+
+TEST(PrimaryTreeIndex,ExactMatchWhenBestMatchLookupIsCalled) {
+    FileTable ft;
+    ft.init("../csv/bnumber2.csv");
+    PrimaryTreeIndex index;
+    index.buildIndex(ft, 0);
+    ASSERT_THAT(index.exactMatch("01386"), Eq(25));      
+}
+
 
 TEST(PrimaryTreeIndex,BestMatchLookup) {
     FileTable ft;
@@ -38,10 +47,18 @@ TEST(PrimaryTreeIndex,BestMatchLookupEnd) {
     ASSERT_THAT(index.bestMatch("019956"), Eq(55));      
 }
 
-TEST(PrimaryTreeIndex,NoMatchLookup) {
+TEST(PrimaryTreeIndex,NoMatchLookupWhenBestMatchIsCalled) {
     FileTable ft;
     ft.init("../csv/bnumber2.csv");
     PrimaryTreeIndex index;
     index.buildIndex(ft, 0);
     ASSERT_THAT(index.bestMatch("0060175559138"), Eq(-1));      
+}
+
+TEST(PrimaryTreeIndex,NoMatchLookupWhenExactMatchIsCalled) {
+    FileTable ft;
+    ft.init("../csv/bnumber2.csv");
+    PrimaryTreeIndex index;
+    index.buildIndex(ft, 0);
+    ASSERT_THAT(index.exactMatch("0060175559138"), Eq(-1));      
 }
