de34c3feadd2763ec04025ee917a71d06bdcad76
spakai <spakai@gmail.com>
2015-02-27 13:40:11 +0800

    Initial implementation of exactMatch

--- SecondaryTreeIndex.cpp
+++ SecondaryTreeIndex.cpp
@@ -12,6 +12,16 @@ int SecondaryTreeIndex::size() const {
 }
 
 std::vector<int> SecondaryTreeIndex::exactMatch(const std::string& key) const {
-    std::vector<int> tmp{-1};
-    return tmp;
+    std::vector<int> values;
+    auto bounds = index.equal_range(key);
+    auto lower_bound = bounds.first;
+    auto upper_bound = bounds.second; 
+
+    if(lower_bound != index.end()) {
+        for(auto it = lower_bound; it != upper_bound; ++it) {
+            values.push_back(it->second);
+        }         
+    } 
+    
+    return values;
 }
