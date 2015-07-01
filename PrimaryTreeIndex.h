07243e6b45a4e95977236aa9a029ceff6c121789
Prakash <spakai@gmail.com>
2015-07-01 23:19:21 +0800

    Initial work to introduce multiple maps

--- PrimaryTreeIndex.h
+++ PrimaryTreeIndex.h
@@ -1,10 +1,19 @@
+#pragma once
+
 #include "Index.h"
 #include "IndexSearchException.h"
 #include <map>
+#include <functional>
 
 template <typename T>
 class PrimaryTreeIndex: public Index {
 	public:
+        PrimaryTreeIndex(std::function<void()> hash)
+            : hash(hash) 
+        {}
+        PrimaryTreeIndex()
+            : hash([]{})
+        {}
         virtual void buildIndex(Table & table, int index_column);
         virtual void buildIndex(Table & table, int index_column, int value_column);
         virtual const T& exactMatch(const std::string& key) const;
@@ -13,4 +22,5 @@ class PrimaryTreeIndex: public Index {
 
 	private:
         std::map<std::string,T> index;
+        std::function<void()> hash;
 };
